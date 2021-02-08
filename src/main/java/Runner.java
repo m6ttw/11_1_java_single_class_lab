edad77089f6755f4c8d8d5c5548eb523248265cd
m6ttw <matthewrhyswilliams@live.com>
2021-02-08 12:27:10 +0000

    specified mars

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,9 +1,9 @@
 public class Runner {
     public static void main(String[] args) {
-        Planet planet = new Planet("Mars", 908973);
-        String name = planet.getName();
-        Integer size = planet.getSize();
+        Planet mars = new Planet("Mars", 908973);
+        String name = mars.getName();
+        Integer size = mars.getSize();
         System.out.println(name + " is " + size);
-        planet.explode();
+        mars.explode();
     }
 }
